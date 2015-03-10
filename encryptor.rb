3aee338ddb684c198ed4d8ce655388a603f80c54
Sarah McManus <mcmanus.sarah.a@gmail.com>
2015-03-10 14:12:55 -0400

    Updated string decrypt - works

--- encryptor.rb
+++ encryptor.rb
@@ -7,25 +7,25 @@ class Encryptor
 		Hash[characters.zip(rotated_characters)]
 	end
 
-	def encrypt_letter(letter)
-		rotation = 13
+	def encrypt_letter(letter,rotation)
 		cipher_for_rotation = cipher(rotation)
 		cipher_for_rotation[letter]
 	end
 
- 	def encrypt(string)
+ 	def encrypt(string,rotation)
  		letters = string.split("")
  		results = letters.collect do |letter|
- 			encrypted_letter = encrypt_letter(letter)
+ 			encrypted_letter = encrypt_letter(letter,rotation)
  		end
  		results.join
  	end 
 
  	#I think I should be able to call encrypt, but didn't work...
- 	def decrypt(string)
+ 	#Also think I should be able to call characters.length instead of 91...
+ 	def decrypt(string,rotation)
  		letters = string.split("")
  		results = letters.collect do |letter|
- 			encrypted_letter = encrypt_letter(letter)
+ 			encrypted_letter = encrypt_letter(letter, 91 - rotation)
  		end
  		results.join
  	end
