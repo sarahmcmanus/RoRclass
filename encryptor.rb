aa808cfb43b2cbeaa198650fab69971f569ca701
Sarah McManus <mcmanus.sarah.a@gmail.com>
2015-03-10 12:49:14 -0400

    Deleted commented-out old code

--- encryptor.rb
+++ encryptor.rb
@@ -13,23 +13,6 @@ class Encryptor
 		cipher_for_rotation[letter]
 	end
 
-=begin
-	def cipher
-	{'a' => 'n', 'b' => 'o', 'c' => 'p', 'd' => 'q',
-     'e' => 'r', 'f' => 's', 'g' => 't', 'h' => 'u',
-     'i' => 'v', 'j' => 'w', 'k' => 'x', 'l' => 'y',
-     'm' => 'z', 'n' => 'a', 'o' => 'b', 'p' => 'c',
-     'q' => 'd', 'r' => 'e', 's' => 'f', 't' => 'g',
-     'u' => 'h', 'v' => 'i', 'w' => 'j', 'x' => 'k',
-     'y' => 'l', 'z' => 'm'}
- 	end
-
- 	def encrypt_letter(letter)
- 		lowercase_letter = letter.downcase
- 		cipher[lowercase_letter]
- 	end
-=end
-
  	def encrypt(string)
  		letters = string.split("")
  		results = letters.collect do |letter|
@@ -46,25 +29,5 @@ class Encryptor
  		end
  		results.join
  	end
-
-# I do believe the following begin/end syntax will do a multiline comment
-# but only if it does not start with a tab 	
-=begin 
- 	def encrypt(string)
- 		# 1. Cut the input string into chars, stores in array called letters
- 		letters = string.split("")
-
- 		# 2. Encrypt those letters one at a time, gathering the results
- 		results = []
- 		# for each string in letters array, store that element to local var letter
- 		# iterate through letters array
- 		letters.each do |letter|
- 			encrypted_letter = encrypt_letter(letter)
- 			# now put the encrypted char in results array
- 			results.push(encrypted_letter)
- 		end
- 		results.join
- 	end
-=end
 
 end
