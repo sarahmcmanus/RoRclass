792c92d98a4d1cbc9d022db9f6f8d4da84160a2f
Sarah McManus <mcmanus.sarah.a@gmail.com>
2015-03-28 12:08:32 -0400

    Updating works

--- idea_box/app.rb
+++ idea_box/app.rb
@@ -28,4 +28,16 @@ class IdeaBoxApp < Sinatra::Base
     Idea.delete(id.to_i)
     redirect '/'
   end
+
+  get '/:id/edit' do |id|
+    idea = Idea.find(id.to_i)
+    erb :edit, locals: {id: id, idea: idea}
+  end
+
+  put '/:id' do |id|
+    data = {title: params['idea_title'], description: params['idea_description']}
+    Idea.update(id.to_i, data)
+    redirect '/'
+  end
+
 end
--- idea_box/idea.rb
+++ idea_box/idea.rb
@@ -41,4 +41,21 @@ class Idea
     end
   end
 
+  def self.find(id)
+    raw_idea = find_raw_idea(id)
+    Idea.new(raw_idea[:title], raw_idea[:description])
+  end
+
+  def self.find_raw_idea(id)
+    database.transaction do
+      database['ideas'].at(id)
+    end
+  end
+
+  def self.update(id, data)
+    database.transaction do
+      database['ideas'][id] = data
+    end
+  end
+
 end
