4752b7c1f3355645c6dcca97dd7a4c4c241e8409
Sarah McManus <mcmanus.sarah.a@gmail.com>
2015-03-28 10:59:58 -0400

    Adding in-progress Web Guesser

---
+++ idea_box/app.rb
@@ -0,0 +1,21 @@
+require './idea'
+
+class IdeaBoxApp < Sinatra::Base
+	not_found do
+		erb :error
+	end
+
+	configure :development do 
+		register Sinatra::Reloader
+	end
+
+	get '/' do
+	  erb :index, locals: {ideas: Idea.all}
+	end
+
+	post '/' do
+		idea = Idea.new(params['idea_title'], params['idea_description'])
+    idea.save
+    redirect '/'
+	end
+end
